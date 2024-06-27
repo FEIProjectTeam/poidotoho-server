cb8c5189a5daa7629d1ab676969b7da50c1ec46e
Viktor Bojda <viktorbojda17@gmail.com>
2024-06-27 21:26:59 +0200

    triv: load SECRET_KEY from environment

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--umzy4k3#+wlb)#=g@z9px#g7ah(z%lq5mjf##7c)59epg&5@d"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
